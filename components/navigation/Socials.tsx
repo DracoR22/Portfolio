f24a3bf173c72117a20cf9c8d3fffd51c87aa6b8
DracoR22 <rdraco039@gmail.com>
2023-11-28 20:12:34 -0500

    socialsfix

--- components/navigation/Socials.tsx
+++ components/navigation/Socials.tsx
@@ -5,7 +5,7 @@ import { FaLinkedin, FaGithub } from "react-icons/fa"
 
 const Socials = () => {
   return (
-    <div className="flex items-center gap-x-5 text-[25px]">
+    <div className="flex z-[99999] items-center gap-x-5 text-[25px]">
         <a href='https://github.com/DracoR22' target="_blank" rel="noopener noreferrer"  className="hover:text-accent transition-all duration-300 cursor-pointer">
           <FaGithub/>
         </a>
